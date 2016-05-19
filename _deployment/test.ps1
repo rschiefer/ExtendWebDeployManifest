4f4336fe05c4298fef12ec2ad187d5d50dbff4cc
Robb Schiefer <rschiefer@live.com>
2016-05-19 11:03:16 -0500

    Initial checkin

---
+++ _deployment/test.ps1
@@ -0,0 +1,8 @@
+﻿$path = 'c:\temp\testingPostSync\'
+if(!(Test-Path -Path $path )){
+    New-Item -ItemType directory -Path $path
+}
+
+$date = Get-date
+Write-Output "date=$date"
+New-Item "$($path)$($date.year).$($date.month).$($date.day).$($date.hour).$($date.minute).$($date.second).txt" -type file
